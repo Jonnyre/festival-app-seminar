ae0f05fc7d997979d07ac376304b92aa8dc5b115
Jonathan <jonathan.rissler@gmx.de>
2022-05-09 18:30:01 +0200

    Init repo

---
+++ index.ts
@@ -0,0 +1,34 @@
+import express, {Request, Response} from "express";
+import { json } from "body-parser";
+import mongoose from "mongoose"
+
+const app = express()
+app.use(json())
+
+mongoose.connect("mongodb+srv://admin:Test123@festivalapp.oheeh.mongodb.net/FestivalDB?retryWrites=true&w=majority");
+const connection = mongoose.connection
+
+app.get("/", [], async (req: Request, res: Response) => {
+    console.log("Request!!!");
+    const collection = connection.db.collection("Festival");
+    const festivals = await collection.find({}).toArray()
+    // const festivals = await Festival.find({});
+    return res.send(festivals)
+})
+
+app.listen(3000, () => {
+    console.log("server running");
+})
+
+const festivalSchema = new mongoose.Schema({
+    name: String,
+    startDate: String,
+    endDate: String,
+    place: String,
+    latitude: Number,
+    longitude: Number,
+    price: Number,
+    ticketCountAvailable: Number
+})
+
+const Festival = mongoose.model("Festival", festivalSchema);
