9a94f45bfe4c0ab35a8f0692f8fa1a0cd004835f
Jonathan <jonathan.rissler@gmx.de>
2022-06-21 10:26:12 +0200

    objectid

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const body_parser_1 = require("body-parser");
 const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const mongodb_1 = __importDefault(require("mongodb"));
+const ObjectID = mongodb_1.default.ObjectID;
 const app = (0, express_1.default)();
 app.use((0, body_parser_1.json)());
 app.use((0, cors_1.default)());
@@ -40,7 +41,7 @@ app.post("/createFestival", [], (req, res) => __awaiter(void 0, void 0, void 0, 
     console.log(req.body);
     const startDateMongo = new Date(req.body.startDate);
     const endDateMongo = new Date(req.body.endDate);
-    const objectId = new mongodb_1.default.ObjectId();
+    const objectId = new ObjectID();
     yield collection.insertOne({ _id: objectId, name: req.body.name, startDate: startDateMongo, endDate: endDateMongo, place: req.body.place, latitude: req.body.latitude, longitude: req.body.longitude, price: req.body.price, ticketCountAvailable: req.body.ticketCountAvailable });
     return res.send();
 }));
